1685e19628306d00d8b78251d24e829e3add2eed
Mrudhula529 <laxmimrudhula.kasparaju@slu.edu>
2025-11-30 18:29:50 +0000

    initial work for lab 06

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import 'tachyons/css/tachyons.min.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './styles.css'; // optional, if you have global styles
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
@@ -15,7 +15,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
